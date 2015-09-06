f7f2fb6b512d28bd63fe7dddda1ab53a82f311bf
woniesong92 <hs459@cornell.edu>
2015-09-06 14:19:55 -0400

    sign up view controller added but unconnected

---
+++ toaster-objc/SignUpViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.h
+//  toaster-objc
+//
+//  Created by Howon Song on 9/6/15.
+//  Copyright (c) 2015 honeyjamstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WebViewManager.h"
+#import "LoadingManager.h"
+
+@interface SignUpViewController : UIViewController <UIWebViewDelegate, WKNavigationDelegate, WKUIDelegate> {
+    WebViewManager *_webViewManager;
+    LoadingManager *_loadingManager;
+}
+@end
---
+++ toaster-objc/SignUpViewController.m
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.m
+//  toaster-objc
+//
+//  Created by Howon Song on 9/6/15.
+//  Copyright (c) 2015 honeyjamstudio. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
