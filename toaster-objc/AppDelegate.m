14dc66464d53ea63488acb365335359dd8938289
woniesong92 <hs459@cornell.edu>
2015-09-11 22:36:15 -0400

    Parse is setup: should be able to receive pushes

--- toaster-objc/AppDelegate.m
+++ toaster-objc/AppDelegate.m
@@ -40,6 +40,7 @@
     // Store the deviceToken in the current installation and save it to Parse.
     PFInstallation *currentInstallation = [PFInstallation currentInstallation];
     [currentInstallation setDeviceTokenFromData:deviceToken];
+    currentInstallation.channels = @[ @"global" ];
     [currentInstallation saveInBackground];
 }
 
