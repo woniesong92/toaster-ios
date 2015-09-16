a2f59692913eb9b9c8cd553205e2a375b8d1848a
woniesong92 <hs459@cornell.edu>
2015-09-16 00:01:27 -0400

    really publishing now

--- toaster-objc/Constants.h
+++ toaster-objc/Constants.h
@@ -1,5 +1,5 @@
-#define BASE_URL @"http://192.168.0.103:3000"
-//#define BASE_URL @"http://104.131.158.80"
+//#define BASE_URL @"http://192.168.0.103:3000"
+#define BASE_URL @"http://104.131.158.80"
 //#define BASE_URL @"http://192.168.1.118:3000"
 #define TRENDING_URL [NSString stringWithFormat:@"%@%@", BASE_URL, @"/trending"]
 #define NEW_POST_URL [NSString stringWithFormat:@"%@%@", BASE_URL, @"/newPost"]
