add2bddd98e6f776be3c94bfa5aca1af8e566759
woniesong92 <hs459@cornell.edu>
2015-10-02 23:27:54 -0400

    making verification page

---
+++ toaster-objc/VerificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  VerificationViewController.h
+//  Toaster
+//
+//  Created by Howon Song on 10/2/15.
+//  Copyright (c) 2015 honeyjamstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VerificationViewController : UIViewController
+
+@end
---
+++ toaster-objc/VerificationViewController.m
@@ -0,0 +1,13 @@
+//
+//  VerificationViewController.m
+//  Toaster
+//
+//  Created by Howon Song on 10/2/15.
+//  Copyright (c) 2015 honeyjamstudio. All rights reserved.
+//
+
+#import "VerificationViewController.h"
+
+@implementation VerificationViewController
+
+@end
